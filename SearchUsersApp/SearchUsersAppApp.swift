aa2e05aba3d8ae5301452cfac130025799e8fb16
Alina Nicorescu <alina.i.nicorescu@gmail.com>
2025-02-11 12:05:29 +0200

    Initial Commit

---
+++ SearchUsersApp/SearchUsersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchUsersAppApp.swift
+//  SearchUsersApp
+//
+//  Created by Alina Nicorescu on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SearchUsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SearchUsersAppTests/SearchUsersAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchUsersAppTests.swift
+//  SearchUsersAppTests
+//
+//  Created by Alina Nicorescu on 11.02.2025.
+//
+
+import Testing
+@testable import SearchUsersApp
+
+struct SearchUsersAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
